6e083e69a44f38ffa9a0ceb5a0399b5179f7c6d5
konarjg <konarskikrzysztof1@gmail.com>
2022-12-03 16:49:22 +0100

    Created the app

---
+++ Alicja/Alicja.Android/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Alicja.Droid
+{
+    [Activity(Label = "Alicja", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        private void Init()
+        {
+            var messages = new List<string>();
+            var message = "";
+
+            using (var reader = new StreamReader(Assets.Open("messages.txt")))
+            {
+                while ((message = reader.ReadLine()) != null)
+                    messages.Add(message);
+            }
+
+            MainPage.Init(messages);
+        }
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            Init();
+        }
+
+        protected override void OnStop()
+        {
+            
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Alicja/Alicja/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace Alicja
+{
+    public partial class MainPage : ContentPage
+    {
+        private static Random Random = new Random();
+        private static List<string> Messages = new List<string>();
+        private static Queue<string> UsedMessages = new Queue<string>();
+        private static int MessagesDisplayed = 0;
+
+        private static MainPage Instance;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Instance = this;
+        }
+
+        public static void Init(List<string> messages)
+        {
+            Messages = messages;
+            DisplayMessage();
+        }
+
+        private static void DisplayMessage()
+        {
+            if (Messages.Count == 0)
+                return;
+
+            var message = Messages[Random.Next(0, Messages.Count)];
+
+            Instance.MessageUI.Text = message;
+            Messages.Remove(message);
+            UsedMessages.Enqueue(message);
+            ++MessagesDisplayed;
+        }
+
+        public static Queue<string> GetUsedMessages()
+        {
+            return UsedMessages;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (MessagesDisplayed == 3)
+            {
+                for (int i = 0; i < 3; ++i)
+                {
+                    var message = UsedMessages.Dequeue();
+                    Messages.Add(message);
+                }
+
+                MessagesDisplayed = 0;
+            }
+
+            DisplayMessage();
+        }
+    }
+}
